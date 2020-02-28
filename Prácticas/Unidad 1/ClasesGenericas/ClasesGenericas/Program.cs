9108a4fabc1f514e2a77f2f77bcbf9d433eb8bea
Joel Reyes Rivera <jreyes1999@hotmail.com>
2020-02-28 12:58:26 -0700

    Clases Genéricas 28/02/2020

---
+++ Prácticas/Unidad 1/ClasesGenericas/ClasesGenericas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClasesGenericas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, String> diccionario = new Dictionary<int, string>();
+            List<String> list = new List<string>();
+
+            diccionario.Add(1, "VALOR 1");
+            diccionario.Add(2, "VALOR 2");
+            diccionario.Add(3, "VALOR 3");
+            diccionario.Add(4, "VALOR 4");
+
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+            list.Add("D");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Prácticas/Unidad 1/PracticaDictionary/AsesoriaDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsesoriaDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
