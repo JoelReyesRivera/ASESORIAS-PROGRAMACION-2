4ec43b6a814e9867a31d37f0bad5470e99dc4a17
Jorge <jorgeosunaqui@gmail.com>
2020-03-26 22:08:26 -0700

    Amonos Recio

---
+++ Prácticas/Unidad 2/Articulos_Visual/Manejadora.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Articulos_Visual
+{
+    public class Manejadora
+    {
+        private Dictionary<int, Producto> productos;
+        //CONSTRUCTOR
+        public Manejadora()
+        {
+            this.productos = new Dictionary<int, Producto>();
+        }
+
+        //AGREGAR PRODUCTO
+        public bool AgregarProducto(int clave, string descripcion, float precioUnitario, int existencia, bool activo)
+        {
+            return false;
+        }
+
+        //ELIMINAR PRODUCTO
+        public int EliminarProducto(int clave)
+        {
+            return 0;
+        }
+
+       //OBTENER LA EXISTENCIA DE UN PRODUCTO
+       public int ObtenerExistencia(int clave)
+        {
+           for(int i = 0; i < productos.Count; i++)
+            {
+                
+            }
+            return -1;
+        }
+
+        //OBTENER PRECIO UNITARIO
+        public float ObtenerPrecio(int clave)
+        {
+            return 1;
+        }
+
+        //BUSCAR PRODUCTO
+        public KeyValuePair<int, Producto> BuscaProducto(int clave)
+        {
+            var temp=new KeyValuePair<int, Producto>();
+            for(int i = 0; i < productos.Count; i++)
+            {
+                
+            }
+            return temp;
+        }
+
+        public bool RestaExistencia(int clave,int numAr)
+        {
+            bool flag = true;
+            var temp = BuscaProducto(clave);
+            if (temp.Value.pExistencia - numAr < 0)
+                flag = true;
+
+            return flag;
+        }
+
+        public bool ValidaProducto(int clave)
+        {
+            return productos.ContainsKey(clave);
+        }
+    }
+}
---
+++ Prácticas/Unidad 2/Articulos_Visual/Producto.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Articulos_Visual
+{
+    public class Producto
+    {
+        //private int clave;
+        private String descripcion;
+        private float precioUnitario;
+        private int existencia;
+        private bool activo;
+
+        //CONSTRUCTOR, (NO SE DEBE IMPLEMENTAR UN CONSTRUCTOR VACÍO)
+        public Producto(string descripcion, float precioUnitario, int existencia, bool activo)
+        {
+            //this.clave = clave;
+            this.descripcion = descripcion;
+            this.precioUnitario = precioUnitario;
+            this.existencia = existencia;
+            this.activo = activo;
+        }
+        //PROPIEDADES
+        /*public int pClave
+        {
+            get
+            {
+                return clave;
+            }
+        }*/
+        public float pPrecioUnitario
+        {
+            get
+            {
+                return precioUnitario;
+            }
+            set
+            {
+                precioUnitario = value;
+            }
+        }
+        public int pExistencia
+        {
+            get
+            {
+                return existencia;
+            }
+            set
+            {
+                existencia = value;
+            }
+        }
+        public String pDescripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+            set
+            {
+                descripcion = value;
+            }
+        }
+
+        public bool pActivo
+        {
+            get
+            {
+                return activo;
+            }
+            set
+            {
+                activo = value;
+            }
+        }
+
+        //REPRESENTACION EN STRING DEL OBJETO
+        override
+        public String ToString()
+        {
+            return "\nDESCRIPCIÓN: " + descripcion + "\nPRECIO UNITARIO: $" + precioUnitario + "\nEXISTENCIA: " + existencia; 
+        }
+    }
+
+}
